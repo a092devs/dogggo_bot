714e4c3c90d164a97f043d2738689b627ff4066a
IshanThakur2004 <51969435+IshanThakur2004@users.noreply.github.com>
2020-02-07 08:55:03 +0530

    Updated main.py
    Changed bop to bork

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ def get_url():
     url = contents['url']
     return url
 
+def get_image_url():
+    allowed_extension = ['jpg','jpeg','png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+    return url
+
 def bork(bot, update):
-    url = get_url()
+    url = get_image_url()
     chat_id = update.message.chat_id
     bot.send_photo(chat_id=chat_id, photo=url)
 
 def main():
-    updater = Updater('941843799:AAH_IkDvzX310eIS-zzv5or0ZVMLRKpdhHoN')
+    updater = Updater('YOUR_TOKEN')
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('bork',bork))
     updater.start_polling()
